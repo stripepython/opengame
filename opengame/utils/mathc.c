43a0202ddd66522ffe902360bac148b5b01bcc21
stripepython <98206176+stripepython@users.noreply.github.com>
2023-01-04 18:39:11 +0800

    Update mathc.c

--- opengame/utils/mathc.c
+++ opengame/utils/mathc.c
@@ -3,26 +3,21 @@ Library written in C language.
 Used to accelerate and replace functions that Python cannot implement.
 
 Compile: gcc -shared -o mathc.dll mathc.c
+In Linux: gcc mathc.c -shared -fPIC -o mathc.so
 */
 
 float InvSqrt(float n)
 {
-	long i;
-	float x2, y;
-	x2 = n * 0.5F;
-	y = n;
-	i = *(long *) &y;
-	//i = 0x5f3759df - (i >> 1);
-	i = 0x5f375a86 - (i >> 1);
-	y = *(float *) &i;
-	y = y * (1.5F - (x2 * y * y));
-
-#ifndef Q3_VM
-#ifdef __linux__
-	assert(!isnan(y)); // bk010122 - FPE?
-#endif
-#endif
-	return y;
+    long i;
+    float x2, y;
+    x2 = n * 0.5F;
+    y = n;
+    i = *(long *) &y;
+    //i = 0x5f3759df - (i >> 1);
+    i = 0x5f375a86 - (i >> 1);
+    y = *(float *) &i;
+    y = y * (1.5F - (x2 * y * y));
+    return y;
 }
 
 int IntegerLog2(int val) {
