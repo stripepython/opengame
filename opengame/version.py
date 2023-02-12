0e89e98e6b44431e0747242a9a9b42b806a2d906
stripepython <98206176+stripepython@users.noreply.github.com>
2023-02-12 12:45:37 +0800

    Update version to 1.1.2

--- opengame/version.py
+++ opengame/version.py
@@ -3,7 +3,7 @@ from collections import namedtuple
 __all__ = ['version', 'get_string']
 
 _Version = namedtuple('Version', ['major', 'minor', 'micro'])
-version = _Version(1, 0, 2)
+version = _Version(1, 1, 2)
 
 def get_string():
     return '.'.join(map(str, version))
